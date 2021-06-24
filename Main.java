680d030fd08ee7130112a3e8d0f4b9c22a5fb31d
Bruna de Andrade Braguin <62024853+BrunaABraguin@users.noreply.github.com>
2021-06-24 21:02:58 +0000

    Add list array

--- Main.java
+++ Main.java
@@ -1,5 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+
+    List<String> nomes = new ArrayList<>();
+
+    nomes.add("Juliana");
+    nomes.add("Pedro");
+    nomes.add("Carlos");
+    nomes.add("Larissa");
+    nomes.add("João");
+
+    System.out.println("Lista --> " + nomes);
+
+    nomes.set(2, "Roberto");
+
+    System.out.println("Carlos por Roberto --> " + nomes);
+
+    System.out.println("Posição 1 --> "+ nomes.get(1));
+
+    nomes.remove(4);
+
+    System.out.println("Remove posição 4 --> "+ nomes);
+
+  
+
   }
 }
